7de59609ac36018b66e777e6196ba97e48758ee6
carlsonl2 <carlsonl2@nycstudents.net>
2026-01-05 08:57:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
-list = []
-import requests
+
+'''import requests
 def getGames(Games):
     response = requests.get(f"https://www.cheapshark.com/api/1.0/games?title={Games.lower()}")
     if response.status_code != 200:
@@ -9,18 +9,23 @@ def getGames(Games):
         
     data = response.json()
 
-    title = [t["type"]["name"] for t in data["external"]]
-    
-    
-    
-which = input("What game do you want")    
-game = getGames(which)
-for index,item in enumerate(list):
-    print(f"{index} : {item[]}")
-   
- 
-
-
+    for i in range(len(data)):
+        return {
+            "title": data[i]['external'],
+            "cheapest_price": data[i]['cheapest'],
+            }
+  
+game = input("Enter Game name: ")
+game = getGames(game)
+if game:
+    for key, value in game.items():
+        print(f"{key} → {value}")'''
+import tkinter as tk # bring in tkinter and call it tk
+# Create the main window (like your app's frame)
+window = tk.Tk()
+window.title("Message Reverser") # title at the top of the window
+window.geometry("400x250") # set the size (width x height)
+window.resizable(False, False) # keep it from being resized
 
 
 
